540063933da44cfa095b87aecb8fc5f19f9642c7
Stream of Mind Soup <streamofmindsoup@gmail.com>
2020-09-12 01:20:09 +0100

    set homepage for hosting on GH

---
+++ docs/precache-manifest.9a8c15d3a71aff2030a302aab091d3b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261ed94af6369de438fd2952c94349b9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6ec33a332027c310b230",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "24bf98668027b87a82ac",
+    "url": "./static/js/2.0ecfa8a0.chunk.js"
+  },
+  {
+    "revision": "8d033527bfde269db1e0fb1aca3c57e8",
+    "url": "./static/js/2.0ecfa8a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ec33a332027c310b230",
+    "url": "./static/js/main.571114c3.chunk.js"
+  },
+  {
+    "revision": "2fed79f27f2282c2fe02",
+    "url": "./static/js/runtime-main.1d91dff4.js"
+  }
+]);
